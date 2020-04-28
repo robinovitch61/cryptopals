c61a4841a583bca77478f7d9248eebb2fc1096ed
Leo Robinovitch <leorobinovitch@gmail.com>
2020-04-28 16:41:01 -0700

    fixed XOR

--- src/main/scala/Set1.scala
+++ src/main/scala/Set1.scala
@@ -5,8 +5,26 @@ import java.math.BigInteger
 class Set1 {
   def returns1 = () => 1
 
-  def hex_to_base64(hex: String): String = {
-    val bytes = new BigInteger(hex, 16).toByteArray
-    Base64.getEncoder().encodeToString(bytes)
+  def hexToBytes(hex: String) = new BigInteger(hex, 16).toByteArray
+
+  def bytesToBase64(bytes: Array[Byte]): String = Base64.getEncoder().encodeToString(bytes)
+
+  def bytesToHex(bytes: Seq[Byte]): String = {
+    val sb = new StringBuilder
+    for (b <- bytes) {
+      sb.append(String.format("%02x", b))
+    }
+    sb.toString
+  }
+
+  def hexToBase64(hex: String): String = {
+    bytesToBase64(hexToBytes(hex))
+  }
+
+  def fixedXor(first: String, second: String): String = {
+    val firstBytes = hexToBytes(first)
+    val secondBytes = hexToBytes(second)
+    val xor = firstBytes.zip(secondBytes).map{ case (a, b) => (a ^ b).toByte }
+    bytesToHex(xor)
   }
 }
--- src/test/scala/Set1Spec.scala
+++ src/test/scala/Set1Spec.scala
@@ -9,9 +9,28 @@ class Set1Spec extends FlatSpec {
     assert (set1.returns1() == 1)
   }
 
-  "hex_to_base64" should "convert example correctly" in {
+  "hexToBytes" should "run correctly" in {
+    assert(set1.hexToBytes("0110").mkString(" ") == "1 16")
+  }
+
+  "bytesToBase64" should "run correctly" in {
+    assert(set1.bytesToBase64(Array(12.toByte, 13.toByte)) == "DA0=")
+  }
+
+  "hexToBase64" should "convert example correctly" in {
     val input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
     val output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
-    assert (set1.hex_to_base64(input) == output)
+    assert (set1.hexToBase64(input) == output)
+  }
+
+  "bytesToHex" should "work" in {
+    assert(set1.bytesToHex(Array(12.toByte, 13.toByte)) == "0c0d")
+  }
+
+  "fixedXor" should "succeed" in {
+    val input1 = "1c0111001f010100061a024b53535009181c"
+    val input2 = "686974207468652062756c6c277320657965"
+    val output = "746865206b696420646f6e277420706c6179"
+    assert (set1.fixedXor(input1, input2) == output)
   }
 }
