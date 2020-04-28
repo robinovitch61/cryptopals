81b728e5c4d74e7a0123f1a48fc7a48840b3fae0
Leo Robinovitch <leorobinovitch@gmail.com>
2020-04-28 13:52:14 -0700

    hex_to_base64

--- src/main/scala/Set1.scala
+++ src/main/scala/Set1.scala
@@ -1,5 +1,12 @@
 package cryptopals.set1
+import java.util.Base64
+import java.math.BigInteger
 
 class Set1 {
   def returns1 = () => 1
+
+  def hex_to_base64(hex: String): String = {
+    val bytes = new BigInteger(hex, 16).toByteArray
+    Base64.getEncoder().encodeToString(bytes)
+  }
 }
--- src/test/scala/Set1Spec.scala
+++ src/test/scala/Set1Spec.scala
@@ -8,4 +8,10 @@ class Set1Spec extends FlatSpec {
   "returns1" should "return 1" in {
     assert (set1.returns1() == 1)
   }
+
+  "hex_to_base64" should "convert example correctly" in {
+    val input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+    val output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+    assert (set1.hex_to_base64(input) == output)
+  }
 }
